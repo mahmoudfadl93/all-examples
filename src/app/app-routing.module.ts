0b37d1d05e581ad234952a16fbff9fb7118c5d39
MahmoudFadl <mfadl4201@gmail.com>
2022-08-28 20:37:07 +0200

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core'
+import {RouterModule, Routes} from '@angular/router'
 
 const routes: Routes = [
   {
     path: 'd-form',
+
     loadChildren: () =>
       import('./dynamic-angular-json/dynamic-angular-json.module').then(
         (m) => m.DynamicAngularJsonModule
-      ),
+      )
   },
   {
     path: 'd-table',
     loadChildren: () =>
       import('./dynamic-table/dynamic-table.module').then(
         (m) => m.DynamicTableModule
-      ),
-  },
-];
+      )
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
