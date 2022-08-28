8c3b4f4bac73dc216aa4a91c2a76a7eb0f0ed718
MahmoudFadl <mfadl4201@gmail.com>
2022-08-28 20:50:49 +0200

    s

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import {NgModule} from '@angular/core'
-import {RouterModule, Routes} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router'
 
 const routes: Routes = [
   {
-
     path: 'd-form',
 
     loadChildren: () =>
